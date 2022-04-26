d0bbf6444b58384b1e2d8003eb58ed9cc43cf5e3
yukemeng <2869432967@qq.com>
2022-04-26 16:34:37 +0800

    "项目建立"

---
+++ src/main/java/com/security/SSOApplication.java
@@ -0,0 +1,17 @@
+package com.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther:yukemeng Date:2022/4/26-04-26-16:27
+ * Description:
+ * version:1.0
+ */
+@SpringBootApplication
+public class SSOApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SSOApplication.class,args);
+    }
+}
